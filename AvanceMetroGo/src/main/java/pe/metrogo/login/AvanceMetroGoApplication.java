f1586097921fbaa2c2e9cc6369f1b645369466a4
jorges071098 <71417093+jorges071098@users.noreply.github.com>
2020-11-11 13:16:32 -0500

    Commit Inicial con Eclipse. Close #1

---
+++ AvanceMetroGo/src/main/java/pe/metrogo/login/AvanceMetroGoApplication.java
@@ -0,0 +1,13 @@
+package pe.metrogo.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanceMetroGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanceMetroGoApplication.class, args);
+	}
+
+}
