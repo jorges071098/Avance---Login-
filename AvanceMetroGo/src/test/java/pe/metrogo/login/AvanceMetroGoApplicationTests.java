6e0c177b9ab718e8c3cda654cc96cd063f813d71
jorges071098 <71417093+jorges071098@users.noreply.github.com>
2020-11-11 13:16:32 -0500

    Commit Inicial con Eclipse

---
+++ AvanceMetroGo/src/test/java/pe/metrogo/login/AvanceMetroGoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.metrogo.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvanceMetroGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
